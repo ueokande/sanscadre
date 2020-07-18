2339ba7c2016c64c9f3a104301fce2ca694c6f59
Shin'ya Ueoka <ueokande@i-beam.org>
2020-07-18 15:05:57 +0900

    Add normalize.css

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "normalize.css";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,4 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("app")
 );
-
