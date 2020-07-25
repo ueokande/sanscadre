f6f7bf2036948d49ed6a5e6bb5a27e87b14fdee7
Shin'ya Ueoka <ueokande@i-beam.org>
2020-07-25 16:35:09 +0900

    Add drop zone to add files

--- src/renderer/Screen.tsx
+++ src/renderer/Screen.tsx
@@ -3,6 +3,12 @@ import AppContext from "./AppContext";
 import styled from "styled-components";
 import * as ipc from "./ipc";
 import PDFReader from "./PDFReader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFileImage,
+  faFileVideo,
+  faFilePdf,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Container = styled.div`
   width: 100%;
@@ -24,6 +30,40 @@ const Video = styled.video`
   height: 100%;
   object-fit: contain;
 `;
+const DropArea = styled.div`
+  background-color: #f8f8f8;
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
+
+const DropAreaInner = styled.div`
+  position: absolute;
+  left: 2rem;
+  right: 2rem;
+  top: 2rem;
+  bottom: 2rem;
+  border: 4px gray dashed;
+  background-color: #f0f0f0;
+  color: #606060;
+  border-radius: 2rem;
+  font-size: 4rem;
+  font-family: sans-serif;
+  font-weight: bold;
+  display: flex;
+  flex-flow: column;
+  justify-content: center; /* align horizontal */
+  align-items: center; /* align vertical */
+`;
+
+const FontAwesomeZone = styled.div`
+  font-size: 150%;
+`;
+
+const IconCenter = styled.span`
+  font-size: 125%;
+  margin: 0 1rem;
+`;
 
 const Screen = () => {
   const { state, dispatch } = React.useContext(AppContext);
@@ -31,7 +71,20 @@ const Screen = () => {
   const content = (() => {
     const page = state.pages[state.active];
     if (!page) {
-      return null;
+      return (
+        <DropArea>
+          <DropAreaInner>
+            <FontAwesomeZone>
+              <FontAwesomeIcon icon={faFileImage} />
+              <IconCenter>
+                <FontAwesomeIcon icon={faFileVideo} />
+              </IconCenter>
+              <FontAwesomeIcon icon={faFilePdf} />
+            </FontAwesomeZone>
+            <div>{"Drop you files here"}</div>
+          </DropAreaInner>
+        </DropArea>
+      );
     }
     if (page.type.startsWith("image/")) {
       return <Img src={page.src} />;
